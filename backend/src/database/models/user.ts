02a7057bf39dd0214b9beaf5e4b6f08cd4f132a9
Arnaud <arnaud-buhl@orange.fr>
2024-04-10 19:43:03 +0200

    feat(update): add plugin unique validator on mongoose
    - add package mongoose-unique-validator

--- backend/src/database/models/user.ts
+++ backend/src/database/models/user.ts
@@ -1,8 +1,11 @@
 import mongoose, { Schema } from 'mongoose';
+import uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
+
+UserSchema.plugin(uniqueValidator);
 
 export const User = mongoose.model('User', UserSchema);
