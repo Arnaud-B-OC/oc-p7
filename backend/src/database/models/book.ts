273e8aebe65b7edc4ba11832294bcc475af0626a
Arnaud <arnaud-buhl@orange.fr>
2024-04-12 16:06:21 +0200

    fix: require imageUrl

--- backend/src/database/models/book.ts
+++ backend/src/database/models/book.ts
@@ -4,7 +4,7 @@ const BookSchema = new Schema({
     userId: { type: String, required: true },
     title: { type: String, required: true },
     author: { type: String, required: true },
-    imageUrl: { type: String },
+    imageUrl: { type: String, required: true },
     year: { type: Number, required: true },
     genre: { type: String, required: true },
     ratings: [
